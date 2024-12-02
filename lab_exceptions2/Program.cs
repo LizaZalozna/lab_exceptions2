e16ac86fe7a125f0b274187bdc55dd940a217422
lizazalozna <liza.zalozna@gmail.com>
2024-12-02 21:25:35 +0200

    regexAndStartCyccle

--- lab_exceptions2/Program.cs
+++ lab_exceptions2/Program.cs
@@ -7,6 +7,15 @@ class Program
 {
     static void Main()
     {
-        
+        Regex regexExtForImage = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+        string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+        foreach (string fileName in files)
+        {
+            string extension = Path.GetExtension(fileName);
+            try
+            {
+
+            }
+        }
     }
 }
